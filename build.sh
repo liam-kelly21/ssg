47e12603ffdc60e7890c598fcba1dd9214cfc170
liam-kelly21 <liamjameskelly21@gmail.com>
2025-07-13 14:52:14 +1200

    Build GitHub Pages site

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "https://github.com/liam-kelly21/ssg"
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from node_utils import *
 import shutil
 import os
 from pathlib import Path
+import sys
 
 TEMPLATE_PATH = "template.html"
 
@@ -23,7 +24,7 @@ def copy_recursive(source,destination):
     
     return
 
-def generate_page(from_path,template_path,dest_path):
+def generate_page(from_path,template_path,dest_path,base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
     #Read source
     try:
@@ -42,6 +43,8 @@ def generate_page(from_path,template_path,dest_path):
     title = extract_title(markdown)
     #Create page
     page = template.replace("{{ Title }}",title).replace("{{ Content }}",content)
+    #set link paths
+    page = page.replace("href=\"/",f"href=\"{base_path}").replace("src=\"/",f"src=\"{base_path}")
 
     try:
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -50,24 +53,28 @@ def generate_page(from_path,template_path,dest_path):
     except IOError as e:
         print(f"Error writing file {dest_path}: {e}")
 
-def generate_pages_recursively(dir_path_content,template_path,dest_dir_path):
+def generate_pages_recursively(dir_path_content,template_path,dest_dir_path,base_path):
     for entry in os.listdir(dir_path_content):
         src = os.path.join(dir_path_content,entry)
         dst = os.path.join(dest_dir_path,entry)
         if os.path.isfile(src):
             dst = Path(dst).with_suffix(".html")
-            generate_page(src,template_path,dst)
+            generate_page(src,template_path,dst,base_path)
         elif os.path.isdir(src):
             if os.path.exists(dst):
                 shutil.rmtree(dst)
             os.mkdir(dst)
-            generate_pages_recursively(src,template_path,dst)
+            generate_pages_recursively(src,template_path,dst,base_path)
         else:
             print("Not a file or directory, skipping")
 
 def main():
-    copy_recursive("static","public")
-    generate_pages_recursively("content",TEMPLATE_PATH,"public")
+    if len(sys.argv) < 2:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1] + "/"
+    copy_recursive("static","docs")
+    generate_pages_recursively("content",TEMPLATE_PATH,"docs",basepath)
 
 if __name__ == "__main__":
     main()
